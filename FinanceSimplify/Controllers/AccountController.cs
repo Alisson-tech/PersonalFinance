eb80965c34311f5edc172e2d61de1556a44fe8b5
aliss <aliss@DESKTOP-QO9AS8C>
2024-12-11 15:15:45 -0300

    Configure controller and librarys

---
+++ FinanceSimplify/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceSimplify.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : Controller
+{
+    [HttpGet]
+    public ActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetId(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public ActionResult Create()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Edit(int id)
+    {
+        return Ok();
+    }
+
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        return Ok();
+    }
+
+}
--- FinanceSimplify/Program.cs
+++ FinanceSimplify/Program.cs
@@ -1,6 +1,19 @@
+using FinanceSimplify.Context;
+using FinanceSimplify.Repositories;
+using FinanceSimplify.Services.Accounts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ContextFinance>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services.AddScoped(provider => new AutoMapper.MapperConfiguration(cfg =>
+{
+    
+}).CreateMapper());
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
